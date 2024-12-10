b3f74e0d362b825ff66c4ab98a4418f6b2b22833
kp9761560@gmail.com <kp9761560@gmail.com>
2024-12-10 22:59:51 +0530

    ProductsNames

--- ques5.js
+++ ques5.js
@@ -1,7 +1,13 @@
-function processProducts(){ 
-let product=[{ name: "Laptop", price: 1000 }, { name: "Mouse", price: 20 }]
-let ans = product.map(product=>product.name);
-
-
- }
- console.log(processProducts(ans))
+// Creating a array of objects 
+let product=[{ name: "Laptop", price: 1000 }, { name: "Mouse", price: 20 }];
+function processProducts(){        // creating  a functions 
+let  productName= product.map((product=>product.name)); // using map for the product names
+product.forEach(product => { // using foreach to iterate products
+    if(product.price>50){   // price is greater than 50 
+        console.log(`${product.name} is above $50 `)
+    } else{                 // price is less than 50
+        console.log(`${product.name} is below $50`)
+    }
+});
+}
+processProducts(product)  //  calling function
